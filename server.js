3e9afab55f40c4a00c7083c18fdb343027793a2b
kvb <kumarivb@gmail.com>
2018-03-02 17:51:16 -0600

    added basic html to survey.html

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use(bodyParser.json());
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT);
 });
