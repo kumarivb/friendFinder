0f83397a8b5347a6304bef5f383db0d8bc0966b1
kvb <kumarivb@gmail.com>
2018-03-02 19:38:21 -0600

    added data to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,114 @@
+var friends = [
+    {
+        name: "Ahmed",
+        photo: "https://cdn.pixabay.com/photo/2016/11/08/05/31/boy-1807545__340.jpg",
+        scores:[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Joan",
+        photo: "https://cdn.pixabay.com/photo/2018/02/09/19/58/composing-3142301__340.jpg",
+        scores:[
+            3,
+            3,
+            2,
+            5,
+            2,
+            5,
+            5,
+            1,
+            2,
+            1
+        ]
+    },
+    {
+        name: "Eric",
+        photo: "https://cdn.pixabay.com/photo/2017/11/12/16/57/sports-2943144__340.jpg",
+        scores:[
+            5,
+            4,
+            3,
+            2,
+            1,
+            1,
+            2,
+            3,
+            4,
+            5
+        ]
+    },
+    {
+        name: "Penelope",
+        photo: "https://cdn.pixabay.com/photo/2016/11/13/00/40/girl-1820122__340.jpg",
+        scores:[
+            1,
+            3,
+            2,
+            5,
+            3,
+            2,
+            3,
+            1,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Edgar",
+        photo: "https://cdn.pixabay.com/photo/2018/01/04/08/04/people-3060107__340.jpg",
+        scores:[
+            2,
+            2,
+            3,
+            2,
+            2,
+            4,
+            3,
+            4,
+            1,
+            5
+        ]
+    },
+    {
+        name: "Samuel",
+        photo: "https://cdn.pixabay.com/photo/2017/12/09/16/41/snow-man-3008179__340.jpg",
+        scores:[
+            4,
+            1,
+            3,
+            5,
+            1,
+            2,
+            1,
+            3,
+            1,
+            1
+        ]
+    },
+    {
+        name: "Winslow",
+        photo: "https://cdn.pixabay.com/photo/2017/11/07/00/07/fantasy-2925250__340.jpg",
+        scores:[
+            5,
+            5,
+            5,
+            5,
+            3,
+            2,
+            1,
+            1,
+            3,
+            4
+        ]
+    }
+]
