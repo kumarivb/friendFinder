8be01243554ce905084ee4e5e315b044ecaa82ad
kvb <kumarivb@gmail.com>
2018-03-02 21:54:05 -0600

    added matching math to apiroutes.js

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -6,4 +6,47 @@ module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friends);
     });
+
+    app.post("/api/friends", function(req, res) {
+        var match = {
+            name: "",
+            photo: "",
+            friendDif: 1000
+        };
+
+        console.log(req.body);
+
+        // survey results
+        var userData = req.body;
+        var userScore = userData.score;
+
+        // have to figure out the differences
+        console.log(userScore);
+
+        var diff = 0;
+
+        // go through friend array
+        for (var i = 0; i < friends.length; i++) {
+            console.log(friends[i]);
+            diff = 0;
+
+            // loop again like in timers, use j-best practice
+            for (var j = 0; i < friends[i].score[j]; j++) {
+                diff += math.abs(parseInt(userScore[j]) - parseInt(friends[i].score[j]));
+
+                if (diff <= match.friendDif) {
+                    match.name = friends[i].name;
+                    match.photo = friends[i].photo;
+                    match.friendDif = diff;
+                };
+            };
+        };
+
+        friends.push(userData);
+
+        res.json(match);
+    });
 }
+
+
+
