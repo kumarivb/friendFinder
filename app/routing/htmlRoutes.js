476f0760f8d5f11b4e3154ad3cfec441976e6cad
kvb <kumarivb@gmail.com>
2018-03-02 18:02:12 -0600

    added routes to htmlroutes.js

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,18 @@
+// dependencies
+var path = require("path");
+
+// routes
+module.exports = function(app) {
+    app.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
+
+    app.get("/home", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"))
+    });
+
+    // default "home"
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"))
+    });
+}
