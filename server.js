938ddb48769877dd6a2b2836fef9798c9265aac4
kvb <kumarivb@gmail.com>
2018-03-03 00:47:35 -0600

    changed port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var path = require("path");
 var app = express();
 
 // Initial port
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
